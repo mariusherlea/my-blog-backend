907791878ccc116cd1b75af0ab8fdee0d218d1d5
marius herlea <mariusherlea@gmail.com>
2026-01-18 13:28:55 +0100

    server.ts

--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+   url: env('PUBLIC_URL'), // 👈 FOARTE IMPORTANT
   app: {
     keys: env.array('APP_KEYS'),
   },
