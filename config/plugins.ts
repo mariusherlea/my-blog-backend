8d6df98b9d753a11c3adef0ea6beacc8ec5e1c2c
marius herlea <mariusherlea@gmail.com>
2026-01-18 13:19:29 +0100

    cloudinary

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,6 +1,19 @@
 //config/plugins.ts
-export default () => ({
+export default ({ env }: { env: (key: string) => string }) => ({
   email: {
     enabled: false,
   },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },}
 });
