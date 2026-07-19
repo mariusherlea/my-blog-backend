96cf9b5a0f9df3a4f6efde8316a63d4eb142b429
marius herlea <mariusherlea@gmail.com>
2026-07-19 09:20:12 +0200

    rehost

--- config/database.ts
+++ config/database.ts
@@ -1,57 +1,12 @@
-// export default ({ env }) => ({
-//   connection: {
-//     client: "postgres",
-//     connection: {
-//       host: env("DATABASE_HOST"),
-//       port: env.int("DATABASE_PORT", 5432),
-//       database: env("DATABASE_NAME"),
-//       user: env("DATABASE_USERNAME"),
-//       password: env("DATABASE_PASSWORD"),
-//       ssl: {
-//         rejectUnauthorized: false,
-//       },
-//     },
-//     pool: {
-//       min: 0,
-//       max: 3, // 🔥 esențial pe Render Free
-//     },
-//   },
-// });
-// export default ({ env }) => ({
-//   connection: {
-//     client: "postgres",
-//     connection: {
-//       host: env("DATABASE_HOST"),
-//       port: env.int("DATABASE_PORT", 5432),
-//       database: env("DATABASE_NAME"),
-//       user: env("DATABASE_USERNAME"),
-//       password: env("DATABASE_PASSWORD"),
-//       ssl: {
-//         rejectUnauthorized: false,}}}})
+//config/database.ts
+import path from "path";
 
-// export default ({ env }) => ({
-//   connection: {
-//     client: "postgres",
-//     connection: {
-//       host: env("DATABASE_HOST", "127.0.0.1"),
-//       port: env.int("DATABASE_PORT", 5432),
-//       database: env("DATABASE_NAME"),
-//       user: env("DATABASE_USERNAME"),
-//       password: env("DATABASE_PASSWORD"),
-//       ssl: env.bool("DATABASE_SSL", false),
-//     },
-//   },
-// });
 export default ({ env }) => ({
   connection: {
-    client: "postgres",
+    client: "sqlite",
     connection: {
-      connectionString: env("DATABASE_URL"),
-      ssl: {
-        rejectUnauthorized: false,
-      },
+      filename: path.join(__dirname, "..", "..", env("DATABASE_FILENAME", ".tmp/data.db")),
     },
+    useNullAsDefault: true,
   },
 });
-
-
